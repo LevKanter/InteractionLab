b1244756b27accf08201b41682587ad58b6cd8c7
Lev Kanter <LevKanter@gmail.com>
2011-09-06 23:46:21 -0400

    style switcher for bill of rights and some preliminary stuff for week 2

---
+++ demos/lab_week2/bill_of_rights/examples/js/style_switcher.js
@@ -0,0 +1,42 @@
+
+	var stylesheets = {
+		Lev: "css/lev.css",
+		Zeke: "css/zeke.css"
+	};
+	
+	jQuery(document).ready(function($) {
+	
+		var activeStyle = $("#active-style");
+		var controls = $("<div class='controls'></div>");
+		var controlsStyle = $("<link rel='stylesheet' href='css/style_switcher.css'>");
+		
+		$("head").append(controlsStyle);
+		
+		controls.hide().appendTo("body").css({ 
+			right: "-100%" 
+		}).show();
+		
+		$.each(stylesheets, function(label, href) {
+			var button = $("<a href='#' class='btn'>"+ label +"</a>");
+			
+			button.click(function(e) {
+				e.preventDefault();
+				activeStyle.attr("href", href);
+				$(this).addClass("active").siblings().removeClass("active");
+			});
+			
+			controls.append(button);
+		});
+		
+		$(document).keydown(function(e) {
+		
+			// the "key code" for the letter "c" is 67
+			
+			if (e.keyCode && e.keyCode == 67) {
+				controls.animate({
+					right: controls.css("right") == "0px" ? "-100%" : 0
+				}, 200);
+			}
+		});
+		
+	});
