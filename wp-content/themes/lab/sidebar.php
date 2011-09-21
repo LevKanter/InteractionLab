7037c5ca868a0e473ad741ebb7ffacca3348cae3
Lev Kanter <LevKanter@gmail.com>
2011-09-21 18:44:07 -0400

    week 4 files

--- wp-content/themes/lab/sidebar.php
+++ wp-content/themes/lab/sidebar.php
@@ -1,4 +1,17 @@
 
 	<div class="sidebar">
-	
+		<?php if (is_front_page()) { ?>
+			<div class="roster">
+				<h2>Students</h2>
+				<ul class="toc">
+					<?php 
+						wp_list_bookmarks(array(
+							"category_name" => "Students",
+							"categorize" => 0,
+							"title_li" => ""
+						));
+					?>
+				</ul>
+			</div>
+		<?php } ?>
 	</div>
